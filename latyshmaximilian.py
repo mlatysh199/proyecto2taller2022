76198b7b2b48850b5c94b4ed4ecda4195f180b34
mlatysh199 <103229540+mlatysh199@users.noreply.github.com>
2022-04-08 12:22:38 -0600

    Corrección
    Eliminación de breaks

--- latyshmaximilian.py
+++ latyshmaximilian.py
@@ -4,35 +4,62 @@
 import sys
 sys.setrecursionlimit((1 << 31) - 1)
 
+#Cada pieza tiene 8 rotaciones
+
+#Pueden haber piezas invalidas.
+
 
 def recibirPieza():
+	pieza, simbolo = convertirPieza()
+	recortarPieza(pieza)
+	return [pieza, simbolo]
+	
+	
+
+def convertirPieza():
 	simbolo = False
 	pieza = []
 	for i in range(4):
 		fila = list(input())
-		if not simbolo:
-			for j in fila:
-				if i != ".":
-					simbolo = j
-					break
-		pieza = pieza + [[j == simbolo for j in fila]]*(simbolo in fila)
-	longitud = len(pieza[0])
-	contador = 0
-	while contador < longitud:
-		noaparecio = True
-		for i in pieza:
-			if i[contador]:
-				noaparecio = False
-				break
-		if noaparecio:
-			for i in range(len(pieza)):
-				pieza[i].pop(contador)
-			longitud -= 1
-		else:
-			contador += 1
-	return [simbolo, pieza]
+		j = 0
+		while j < 4 and not simbolo:
+			if fila[j] != ".":
+				simbolo = fila[j]
+			j += 1
+		pieza.append([j == simbolo for j in fila])
+	return pieza, simbolo
 
-# que mama mitrasos
+
+def recortarPiezaAux(pieza, foc):
+	longitudhoriz = len(pieza[0])*(not foc) + len(pieza)*foc
+	longitudverti = len(pieza[0])*(foc) + len(pieza)*(not foc)
+	i = 0
+	while i < longitudhoriz:
+		noaparecio = True
+		j = 0
+		while j < longitudverti and noaparecio:
+			if foc:
+				if pieza[i][j]:
+					noaparecio = False
+			elif pieza[j][i]:
+				noaparecio = False
+			j += 1
+		if noaparecio:
+			if foc:
+				pieza.pop(i)
+			else:
+				for j in range(longitudverti):
+					pieza[j].pop(i)
+			longitudhoriz -= 1
+		else:
+			i += 1
+
+
+def recortarPieza(pieza):
+	recortarPiezaAux(pieza, True)
+	recortarPiezaAux(pieza, False)
+
+
 if __name__ == "__main__":
 	info = [int(i) for i in input().split(" ")]
 	piezas = [recibirPieza() for i in range(info[2])]
